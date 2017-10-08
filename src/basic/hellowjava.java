5a261153b6dc28196ec262ca647083fd2bf3bdc7
chide <chide@DESKTOP-SQ6FNHF>
2017-10-08 16:24:32 +0530

    myfirstcommit

---
+++ src/basic/hellowjava.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class hellowjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("J");
+	}
+
+}
---
+++ src/helloworld/sample.java
@@ -0,0 +1,8 @@
+package helloworld;
+
+public class sample {
+	public static void main(String[] args) {
+		System.out.println("H");
+	}
+
+}
